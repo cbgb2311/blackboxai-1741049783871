50db84ec65ddd79d1abd714dce8b1d9cb1ea47aa
cbgb2311 <CGARCIABE2398@ucvvirtual.edu.pe>
2025-03-03 19:56:35 -0500

    Add pos/controllers/Admin.php 1741049794715

---
+++ pos/controllers/Admin.php
@@ -0,0 +1,14 @@
+<?php
+class Admin extends Controller{
+    public function __construct() {
+        parent::__construct();
+        session_start();
+    }
+    public function index()
+    {
+        $data['title'] = 'Panel Administrativo';
+        $this->views->getView('admin', 'home', $data);
+    }
+}
+
+?>
