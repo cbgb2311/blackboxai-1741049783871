ead03803646d83fb45f0f1c76ece5b42236ecaee
cbgb2311 <CGARCIABE2398@ucvvirtual.edu.pe>
2025-03-03 19:56:42 -0500

    Add pos/config/Config.php 1741049802164

---
+++ pos/config/Config.php
@@ -0,0 +1,9 @@
+<?php
+define('BASE_URL', 'http://localhost:8080/pos/');
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DBNAME', 'sistema');
+define('CHARSET', '');
+define('TITLE', 'POS VENTA');
+?>
