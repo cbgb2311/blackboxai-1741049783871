d0c6062f34f6b681fe9f0817c8f905126df2adfa
cbgb2311 <CGARCIABE2398@ucvvirtual.edu.pe>
2025-03-03 19:56:35 -0500

    Add pos/controllers/Usuarios.php 1741049795547

---
+++ pos/controllers/Usuarios.php
@@ -0,0 +1,14 @@
+<?php
+class Usuarios extends Controller{
+    public function __construct() {
+        parent::__construct();
+        session_start();
+    }
+    public function index()
+    {
+        $data['title'] = 'Usuarios';
+        $this->views->getView('usuarios', 'index', $data);
+    }
+}
+
+?>
